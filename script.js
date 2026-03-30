72da1f96049677dc83f67fd0eb00331c3f961505
Mohd Imran Ahmad <119959048+Mohd-Imran-Ahmad@users.noreply.github.com>
2026-03-30 09:57:16 +0530

    third

--- script.js
+++ script.js
@@ -1,9 +1,1 @@
-function toggleTeam() {
-    var box = document.getElementById("teamBox");
 
-    if (box.style.display === "none") {
-        box.style.display = "block";
-    } else {
-        box.style.display = "none";
-    }
-}
