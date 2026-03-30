3e86f561d751c3b36954f479708e5f1e18e4af58
Md-Imran-Ahmad <imranbarkati202@gmail.com>
2026-03-30 09:18:34 +0530

    first

---
+++ script.js
@@ -0,0 +1,9 @@
+function toggleTeam() {
+    var box = document.getElementById("teamBox");
+
+    if (box.style.display === "none") {
+        box.style.display = "block";
+    } else {
+        box.style.display = "none";
+    }
+}
